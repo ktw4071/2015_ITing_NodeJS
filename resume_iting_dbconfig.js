b082c5f8e4cb8d0e208c8be0c164501c3ecee72f
ktw4071 <ktw4071@gmail.com>
2016-11-13 22:13:29 -0500

    Add files via upload
    database setting for local and remote server connection

---
+++ resume_iting_dbconfig.js
@@ -0,0 +1,9 @@
+var dbSetting = {
+	connectionLimit : 100,
+	host : 'iting.cilcukctmsxk.ap-northeast-1.rds.amazonaws.com',
+	user : '****',
+	password : '****',
+	database : '****'
+};
+
+module.exports = dbSetting;
